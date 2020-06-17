7f3b410c3cec9369f7f0fb7c8ac5cc2f5d19edfe
Kenji Shirase <juvenile.games0630@gmail.com>
2020-06-17 14:44:26 +0900

    first commit

---
+++ sample_app/config/initializers/application_controller_renderer.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+Refile.secret_key = 'bc619bf33f8cd6f9e060ccaedf20184a6cd28512da5ae9e397ccdc7d58be582d754a10aee5460603828303109e3d28ec763f0f88ca2b9fdcf6b4949a236bc2dc'
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
